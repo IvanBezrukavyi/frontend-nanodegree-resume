26da70faf787e114409eb59db72a4465b81de1e1
IvanBezrukavyi <ivanbezrukavyi1@gmail.com>
2016-11-04 12:27:52 +0200

    first commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,3 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append("Ivan Bezrukavyi");
+var awesomeThoughts = "I am Ivan Bezrukavyi and I am AWESOME!";
+console.log(awesomeThoughts);
